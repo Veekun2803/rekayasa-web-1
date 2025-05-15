55f371e1aeecf56cbd12bc6ebb2a120e919013d6
Veekun2803 <veekunyt@gmail.com>
2025-05-15 15:07:54 +0700

    Add files via upload

---
+++ rekayasa-web-1/tampilWisata.php
@@ -0,0 +1,33 @@
+<?php
+function curl($url){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($ch);
+    curl_close($ch);
+    return $output;
+}
+
+$send = curl("http://localhost/pratikumrekayasaweb/json/getWisata.php");
+$data = json_decode($send, TRUE);
+
+// Tampilkan sebagai tabel
+echo "<h2>Data Wisata</h2>";
+echo "<table border='1' cellpadding='8' cellspacing='0'>";
+echo "<tr>
+        <th>ID Wisata</th>
+        <th>Kota</th>
+        <th>Landmark</th>
+        <th>Tarif</th>
+      </tr>";
+
+foreach($data as $row){
+    echo "<tr>";
+    echo "<td>".$row["id_wisata"]."</td>";
+    echo "<td>".$row["kota"]."</td>";
+    echo "<td>".$row["landmark"]."</td>";
+    echo "<td>".$row["tarif"]."</td>";
+    echo "</tr>";
+}
+echo "</table>";
+?>
